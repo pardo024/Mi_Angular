8857cd06dad7441b51e836ea741bef1d30ccb48b
pardo024 <okaypardo@gmail.com>
2022-04-02 12:48:04 -0600

    z
    z

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador/contador.module';
+import { PokemonModule } from './pokemon/pokemon.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PokemonModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
